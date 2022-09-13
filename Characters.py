f4cd3fc5ac7e38f7bd0504a42a823190e795317c
alex giovannini <alexgiovannini@alexs-MacBook-Air.local>
2022-09-12 23:42:23 -0700

    Created 1 hero and 3 villain dictionaries with various data structures inside

---
+++ Characters.py
@@ -0,0 +1,51 @@
+my_hero = {
+    "name": "Link",
+    "level": 1,
+    "health": 150,
+    "equipment": {"Master Sword", "Slingshot", "Boomerang", "Triforce of Courage"},
+    "attack": (("Slash", 40), ("Slingshot", 20), ("Boomerang", 30)),
+    "Coins": {
+        "Green rupies": 20,
+        "Red rupies": 0,
+        "Purple rupies": 0,
+    }
+}
+
+Villain_1 = {
+    "name": "Volvogia",
+    "level": 5,
+    "health": 100,
+    "equipment": {"Megaton Hammer, Fire tunic, Mirror shield"},
+    "attack": (("Tail whip", 10), ("Fire breath", 15), ("Bite", 20)),
+    "Coins": {
+        "Green rupies": 20,
+        "Red rupies": 20,
+        "Purple rupies": 5,
+    }
+}
+
+villain_2 = {
+    "name": "Twinrova",
+    "level": 10,
+    "health": 200,
+    "equipment": {"Light arrows, Ice arrows, Hover shoes"},
+    "attack": (("Ray of fire", 15), ("Ray of ice", 15), ("Elemental blast", 30)),
+    "Coins": {
+        "Green rupies": 60,
+        "Red rupies": 50,
+        "Purple rupies": 20,
+    }
+}
+
+villain_3 = {
+    "name": "Gannon",
+    "level": 20,
+    "health": 500,
+    "equipment": {"Triforce of power", {"Princess Zelda", "Triforce of Wisdom"}, "Hover shoes", },
+    "attack": (("Slash", 20), ("Stomp", 30), ("Demon Breath", 40)),
+    "Coins": {
+        "Green rupies": 0,
+        "Red rupies": 0,
+        "Purple rupies": 100,
+    }
+}
