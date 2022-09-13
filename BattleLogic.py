3eceb27e92f80c97794147e4164f1067b7601d55
alex giovannini <alexgiovannini@alexs-MacBook-Air.local>
2022-09-13 01:51:49 -0700

    Added logic for 'looting'

--- BattleLogic.py
+++ BattleLogic.py
@@ -1,21 +1,29 @@
 import random
 from Characters import my_hero
-from Characters import Villain_1
+from Characters import villain_1
 from Characters import villain_2
 from Characters import villain_3
 
+def add_inventory(villain):
+    my_hero['equipment'].update(villain['equipment'])
+    for inventory_item in villain['equipment']:
+        print(f"+ {inventory_item} added to {my_hero['name']}'s inventory.")
+        print("")
+
+
 def battle(villain):
     while my_hero['health'] > 0:
-        my_attack = random.choice(my_hero['attack'])
+        my_attack = random.choice(my_hero['attacks'])
         villain['health'] -= my_attack[1]
         if villain['health'] <= 0:
             print(f"{my_hero['name']} has defeated {villain['name']}!")
             print("")
+            add_inventory(villain)
             break
         else:
             print(f"{my_hero['name']} attacked with a {my_attack[0]} causing {my_attack[1]} HP of damage. {villain['name']} has {villain['health']} health left.")
             print("")
-        villain_attack = random.choice(villain['attack'])
+        villain_attack = random.choice(villain['attacks'])
         my_hero['health'] -= villain_attack[1]
         if my_hero['health'] <= 0:
             print("You have died")
@@ -24,11 +32,16 @@ def battle(villain):
             print(f"{villain['name']} attacked with a {villain_attack[0]} causing {villain_attack[1]} HP of damage. {my_hero['name']} has {my_hero['health']} health left.")
             print("")
 
-print("")
-print(f"{my_hero['name']}, you wield the triforce of courage. \nYou must embark on a quest to defeat Gannon, who holds the triforce of power, \nand rescue princess zelda, who holds the triforce of wisdom.")
-print("")
-print(f"{my_hero['name']} has traveled to fire mountain and encountered {Villain_1['name']} (Level: {Villain_1['level']} Health: {Villain_1['health']})")
-print("")
-battle(Villain_1)
-battle(villain_2)
-battle(villain_3)
+def run_game():
+    print("")
+    print(f"{my_hero['name']}, you wield the triforce of courage. \nYou must embark on a quest to defeat Gannon, who holds the triforce of power, \nand rescue princess zelda, who holds the triforce of wisdom.")
+    print("")
+    print(f"{my_hero['name']} has traveled to fire mountain and encountered {villain_1['name']} (Level: {villain_1['level']} Health: {villain_1['health']})")
+    print("")
+    battle(villain_1)
+    print(f"{my_hero['name']} has traveled to the spirit temple and encountered {villain_2['name']} (Level: {villain_2['level']} Health: {villain_2['health']})")
+    print("")
+    battle(villain_2)
+    print(f"{my_hero['name']} has traveled to Gannon's castle and encountered {villain_3['name']} (Level: {villain_3['level']} Health: {villain_3['health']})")
+    print("")
+    battle(villain_3)
--- Characters.py
+++ Characters.py
@@ -3,7 +3,7 @@ my_hero = {
     "level": 1,
     "health": 500,
     "equipment": {"Master Sword", "Slingshot", "Boomerang", "Triforce of Courage"},
-    "attack": (("Slash", 40), ("Slingshot", 20), ("Boomerang", 30)),
+    "attacks": (("Slash", 40), ("Slingshot", 20), ("Boomerang", 30)),
     "Coins": {
         "Green rupies": 20,
         "Red rupies": 0,
@@ -11,12 +11,12 @@ my_hero = {
     }
 }
 
-Villain_1 = {
+villain_1 = {
     "name": "Volvogia",
     "level": 5,
     "health": 100,
-    "equipment": {"Megaton Hammer, Fire tunic, Mirror shield"},
-    "attack": (("Tail whip", 10), ("Fire breath", 15), ("Bite", 20)),
+    "equipment": {"Megaton Hammer", "Fire tunic", "Mirror shield"},
+    "attacks": (("Tail whip", 10), ("Fire breath", 15), ("Bite", 20)),
     "Coins": {
         "Green rupies": 20,
         "Red rupies": 20,
@@ -28,8 +28,8 @@ villain_2 = {
     "name": "Twinrova",
     "level": 10,
     "health": 200,
-    "equipment": {"Light arrows, Ice arrows, Hover shoes"},
-    "attack": (("Ray of fire", 15), ("Ray of ice", 15), ("Elemental blast", 30)),
+    "equipment": {"Light arrows", "Ice arrows", "Hover shoes"},
+    "attacks": (("Ray of fire", 15), ("Ray of ice", 15), ("Elemental blast", 30)),
     "Coins": {
         "Green rupies": 60,
         "Red rupies": 50,
@@ -42,7 +42,7 @@ villain_3 = {
     "level": 20,
     "health": 500,
     "equipment": {"Triforce of power", ("Princess Zelda", "Triforce of Wisdom"), "Hover shoes", },
-    "attack": (("Slash", 20), ("Stomp", 30), ("Demon Breath", 40)),
+    "attacks": (("Slash", 20), ("Stomp", 30), ("Demon Breath", 40)),
     "Coins": {
         "Green rupies": 0,
         "Red rupies": 0,
