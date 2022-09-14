04397a2c7754fadab56b8e63747eb25df8cd0625
alex giovannini <alexgiovannini@alexs-Air.lan>
2022-09-14 13:24:44 -0700

    Added color, refactored text to variable to make code more legible and neat

--- BattleLogic.py
+++ BattleLogic.py
@@ -1,3 +1,5 @@
+from ast import For
+from audioop import add
 import random
 import sys, time
 from colorama import Fore
@@ -6,105 +8,115 @@ from Characters import villain_1
 from Characters import villain_2
 from Characters import villain_3
 
+start_text = f"{Fore.WHITE}{my_hero['name']}, you wield the triforce of courage. \nYou must embark on a quest to defeat Gannon, an evil demon who holds the triforce of power, \nand rescue princess zelda, who holds the triforce of wisdom."
+fire_mountain_text = f"{Fore.LIGHTRED_EX}{my_hero['name']} has traveled to fire mountain and encountered {villain_1['name']}, a giant fire breathing serpent. (Level: {villain_1['level']} Health: {villain_1['health']})"
+spirit_temple_text = f"{Fore.CYAN}{my_hero['name']} has traveled to the spirit temple and encountered {villain_2['name']}, magical witches wielding the elements. (Level: {villain_2['level']} Health: {villain_2['health']})"
+gannons_castle_text = f"{Fore.RED}{my_hero['name']} has traveled to Gannon's castle and encountered {villain_3['name']} (Level: {villain_3['level']} Health: {villain_3['health']})"
+epilogue_text = f"{Fore.YELLOW}Congratulations, you have defeated Gannon and regained the triforce of power!\nYou, along with Princess Zelda and her triforce of wisdom \nwill keep balance and peace in Hyrule."
+
 def slow_print(string):
     for letter in string:
         sys.stdout.write(letter)
         sys.stdout.flush()
-        time.sleep(0.025)
+        time.sleep(0)
     print("")
 
 def battle(villain):
     while my_hero['health'] > 0:
         my_attack = random.choice(my_hero['attacks'])
         villain['health'] -= my_attack[1]
+        
+        my_killing_blow_text = f"{Fore.GREEN}{my_hero['name']} attacked with a {my_attack[0]} ({my_attack[1]} DMG). {villain['name']} has 0 health left."
+        my_attack_text = f"{Fore.GREEN}{my_hero['name']} attacked with a {my_attack[0]} ({my_attack[1]} DMG). {villain['name']} has {villain['health']} health left."
+        victory_text = f"{Fore.GREEN}{my_hero['name']} has defeated {villain['name']}!"
+
         if villain['health'] <= 0:
-            slow_print(f"{my_hero['name']} attacked with a {my_attack[0]} ({my_attack[1]} DMG). {villain['name']} has 0 health left.")
+            slow_print(my_killing_blow_text)
             slow_print("")
-            slow_print(f"{my_hero['name']} has defeated {villain['name']}!")
+            slow_print(victory_text)
             slow_print("")
             break
         else:
-            slow_print(f"{my_hero['name']} attacked with a {my_attack[0]} ({my_attack[1]} DMG). {villain['name']} has {villain['health']} health left.")
+            slow_print(my_attack_text)
             slow_print("")
+
         villain_attack = random.choice(villain['attacks'])
         my_hero['health'] -= villain_attack[1]
+        
+        villain_attack_text = f"{Fore.RED}{villain['name']} attacked with a {villain_attack[0]} ({villain_attack[1]} DMG). {my_hero['name']} has {my_hero['health']} health left."
+        villain_killing_blow_text = f"{Fore.RED}{villain['name']} attacked with a {villain_attack[0]} ({villain_attack[1]} DMG). {my_hero['name']} has 0 health left."
+        
         if my_hero['health'] <= 0:
-            slow_print(f"{villain['name']} attacked with a {villain_attack[0]} ({villain_attack[1]} DMG). {my_hero['name']} has 0 health left.")
+            slow_print(villain_killing_blow_text)
             slow_print("")
-            slow_print("You have died")
+            death()
         else:
-            slow_print(f"{villain['name']} attacked with a {villain_attack[0]} ({villain_attack[1]} DMG). {my_hero['name']} has {my_hero['health']} health left.")
+            slow_print(villain_attack_text)
             slow_print("")
+
+def death():
+    print(f"{Fore.RED}You have died...")
+    sys.exit(f"{Fore.YELLOW}You must attempt your adventure again")
 
 def add_inventory(villain):
     my_hero['equipment'].update(villain['equipment'])
     for inventory_item in villain['equipment']:
-        slow_print(f"+ {inventory_item} added to {my_hero['name']}'s inventory.")
+        slow_print(f"{Fore.WHITE}+ {inventory_item} added to {my_hero['name']}'s inventory.")
 
 def loot_rupies(villain):
+    ruppies_update_text = f"{Fore.WHITE}{my_hero['name']}'s wallet now contains: \n{my_hero['rupies']}"
     for each in my_hero['rupies']:
         my_hero['rupies'][each] += villain['rupies'][each]
         slow_print(f"+ {villain['rupies'][each]} {each}")
     slow_print("")
-    slow_print(f"{my_hero['name']}'s wallet now contains: \n{my_hero['rupies']}")
+    slow_print(ruppies_update_text)
 
 def level_up():
     health_increase = random.randrange(30, 50) * my_hero['level']
     my_hero['level'] += 1
     my_hero['health'] += health_increase
-    slow_print(f"{my_hero['name']}'s level has increased to {my_hero['level']},\nand {my_hero['name']}'s health has increased by {health_increase}. \nTotal health is now {my_hero['health']}.")
-    new_attack_name = input(f"Please name your new attack with your looted weapon: ")
+    slow_print(f"{Fore.WHITE}{my_hero['name']}'s level has increased to {my_hero['level']},\nand {my_hero['name']}'s health has increased by {health_increase}. \nTotal health is now {my_hero['health']}.")
+    print("")
+    new_attack_name = input(f"{Fore.WHITE}Please name your new attack with your looted weapon: ")
     new_attack_tuple = ((new_attack_name, 35 * my_hero['level']),)
     my_hero['attacks'] += new_attack_tuple
 
 def remove_useless_attacks():
-    slow_print("Defeating Gannon will not be easy, you should discard your childhood weapons and use what you have gained on your journey")
+    slow_print(f"{Fore.WHITE}Defeating Gannon will not be easy, you should discard your childhood weapons and use what you have gained on your journey")
     list_of_attacks = list(my_hero['attacks'])
     removed_attack_1 = list_of_attacks.pop(1)
     removed_attack_2 = list_of_attacks.pop(1)
     my_hero['attacks'] = tuple(list_of_attacks)
-    slow_print(f"{removed_attack_1} and {removed_attack_2} have been removed")
+    slow_print(f"{removed_attack_1} and {removed_attack_2} have been removed from your attacks.")
+
+def battle_victory(villain):
+    add_inventory(villain)
+    print("")
+    loot_rupies(villain)
+    print("")
+    level_up()
 
 def run_game():
     slow_print("")
-    slow_print(f"{my_hero['name']}, you wield the triforce of courage. \nYou must embark on a quest to defeat Gannon, an evil demon who holds the triforce of power, \nand rescue princess zelda, who holds the triforce of wisdom.")
+    slow_print(start_text)
     slow_print("")
-    slow_print(f"{my_hero['name']} has traveled to fire mountain and encountered {villain_1['name']}, a giant fire breathing serpent. (Level: {villain_1['level']} Health: {villain_1['health']})")
+    slow_print(fire_mountain_text)
     slow_print("")
     battle(villain_1)
-    if my_hero['health'] <= 0:
-        sys.exit("You must attempt your adventure again")
-    add_inventory(villain_1)
+    battle_victory(villain_1)
     slow_print("")
-    loot_rupies(villain_1)
-    slow_print("")
-    level_up()
-    slow_print("")
-    slow_print(f"{my_hero['name']} has traveled to the spirit temple and encountered {villain_2['name']}, magical witches wielding the elements. (Level: {villain_2['level']} Health: {villain_2['health']})")
+    slow_print(spirit_temple_text)
     slow_print("")
     battle(villain_2)
-    if my_hero['health'] <= 0:
-        sys.exit("You must attempt your adventure again")
-    add_inventory(villain_2)
-    slow_print("")
-    loot_rupies(villain_2)
-    slow_print("")
-    level_up()
+    battle_victory(villain_2)
     slow_print("")
     remove_useless_attacks()
     slow_print("")
-    slow_print(f"{my_hero['name']} has traveled to Gannon's castle and encountered {villain_3['name']} (Level: {villain_3['level']} Health: {villain_3['health']})")
+    slow_print(gannons_castle_text)
     slow_print("")
     battle(villain_3)
-    if my_hero['health'] <= 0:
-        sys.exit("You must attempt your adventure again")
-    add_inventory(villain_3)
+    battle_victory(villain_3)
     slow_print("")
-    loot_rupies(villain_3)
+    slow_print(epilogue_text)
     slow_print("")
-    level_up()
-    if my_hero['health'] > 0:
-        slow_print("")
-        slow_print("Congratulations, you have defeated Gannon and regained the triforce of power!\nYou, along with Princess Zelda and her triforce of wisdom \nwill keep balance and peace in Hyrule.")
-        slow_print("")
     
--- Characters.py
+++ Characters.py
@@ -1,3 +1,5 @@
+from colorama import Fore
+
 my_hero = {
     "name": "Link",
     "level": 1,
@@ -5,47 +7,47 @@ my_hero = {
     "equipment": {"Master Sword", "Slingshot", "Boomerang", "Triforce of Courage"},
     "attacks": (("Slash", 40), ("Slingshot", 20), ("Boomerang", 30)),
     "rupies": {
-        "Green rupies": 20,
-        "Red rupies": 0,
-        "Purple rupies": 0,
+        f"Green rupies": 20,
+        f"Red rupies": 0,
+        f"Purple rupies": 0,
     }
 }
 
 villain_1 = {
     "name": "Volvogia",
     "level": 2,
-    "health": 100,
+    "health": 150,
     "equipment": {"Megaton Hammer", "Fire tunic", "Mirror shield"},
     "attacks": (("Tail whip", 10), ("Fire breath", 15), ("Bite", 20)),
     "rupies": {
-        "Green rupies": 20,
-        "Red rupies": 20,
-        "Purple rupies": 5,
+        f"Green rupies": 20,
+        f"Red rupies": 20,
+        f"Purple rupies": 5,
     }
 }
 
 villain_2 = {
     "name": "Twinrova",
     "level": 3,
-    "health": 200,
+    "health": 250,
     "equipment": {"Light arrows", "Hover shoes"},
     "attacks": (("Ray of fire", 15), ("Ray of ice", 15), ("Elemental blast", 30)),
     "rupies": {
-        "Green rupies": 60,
-        "Red rupies": 50,
-        "Purple rupies": 20,
+        f"Green rupies": 60,
+        f"Red rupies": 50,
+        f"Purple rupies": 20,
     }
 }
 
 villain_3 = {
     "name": "Gannon",
     "level": 4,
-    "health": 500,
+    "health": 450,
     "equipment": {"Triforce of power", ("Princess Zelda", "Triforce of Wisdom"), "Hover shoes", },
     "attacks": (("Slash", 20), ("Stomp", 30), ("Demon Breath", 40)),
     "rupies": {
-        "Green rupies": 0,
-        "Red rupies": 0,
-        "Purple rupies": 100,
+        f"Green rupies": 0,
+        f"Red rupies": 0,
+        f"Purple rupies": 100,
     }
 }
