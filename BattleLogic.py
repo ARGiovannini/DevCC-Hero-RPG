306e33d3bb3fda1fda828e040702ff563ff9b159
alex giovannini <alexgiovannini@alexs-Air.lan>
2022-09-13 18:05:12 -0700

    Changed some text, and moved functions for chronological readability

--- BattleLogic.py
+++ BattleLogic.py
@@ -9,36 +9,8 @@ def slow_print(string):
     for letter in string:
         sys.stdout.write(letter)
         sys.stdout.flush()
-        time.sleep(0.015)
+        time.sleep(0.025)
     print("")
-
-def add_inventory(villain):
-    my_hero['equipment'].update(villain['equipment'])
-    for inventory_item in villain['equipment']:
-        slow_print(f"+ {inventory_item} added to {my_hero['name']}'s inventory.")
-
-def loot_rupies(villain):
-    for each in my_hero['rupies']:
-        my_hero['rupies'][each] += villain['rupies'][each]
-        slow_print(f"+ {villain['rupies'][each]} {each}")
-    slow_print(f"{my_hero['name']}'s wallet now contains: \n{my_hero['rupies']}")
-
-def level_up(villain):
-    health_increase = random.randrange(30, 50) * my_hero['level']
-    my_hero['level'] += 1
-    my_hero['health'] += health_increase
-    slow_print(f"{my_hero['name']}'s level has increased to {my_hero['level']},\nand {my_hero['name']}'s health has increased by {health_increase}. \nTotal health is now {my_hero['health']}.")
-    new_attack_name = input(f"Please name your new attack with your looted weapon: ")
-    new_attack_tuple = ((new_attack_name, 35 * my_hero['level']),)
-    my_hero['attacks'] += new_attack_tuple
-
-def remove_useless_attacks():
-    slow_print("Defeating Gannon will not be easy, you should discard your childhood weapons and use what you have gained on your journey")
-    list_of_attacks = list(my_hero['attacks'])
-    removed_attack_1 = list_of_attacks.pop(1)
-    removed_attack_2 = list_of_attacks.pop(1)
-    my_hero['attacks'] = tuple(list_of_attacks)
-    slow_print(f"{removed_attack_1} and {removed_attack_2} have been removed")
 
 def battle(villain):
     while my_hero['health'] > 0:
@@ -59,11 +31,40 @@ def battle(villain):
             slow_print(f"{villain['name']} attacked with a {villain_attack[0]} ({villain_attack[1]} DMG). {my_hero['name']} has {my_hero['health']} health left.")
             slow_print("")
 
+def add_inventory(villain):
+    my_hero['equipment'].update(villain['equipment'])
+    for inventory_item in villain['equipment']:
+        slow_print(f"+ {inventory_item} added to {my_hero['name']}'s inventory.")
+
+def loot_rupies(villain):
+    for each in my_hero['rupies']:
+        my_hero['rupies'][each] += villain['rupies'][each]
+        slow_print(f"+ {villain['rupies'][each]} {each}")
+    slow_print("")
+    slow_print(f"{my_hero['name']}'s wallet now contains: \n{my_hero['rupies']}")
+
+def level_up():
+    health_increase = random.randrange(30, 50) * my_hero['level']
+    my_hero['level'] += 1
+    my_hero['health'] += health_increase
+    slow_print(f"{my_hero['name']}'s level has increased to {my_hero['level']},\nand {my_hero['name']}'s health has increased by {health_increase}. \nTotal health is now {my_hero['health']}.")
+    new_attack_name = input(f"Please name your new attack with your looted weapon: ")
+    new_attack_tuple = ((new_attack_name, 35 * my_hero['level']),)
+    my_hero['attacks'] += new_attack_tuple
+
+def remove_useless_attacks():
+    slow_print("Defeating Gannon will not be easy, you should discard your childhood weapons and use what you have gained on your journey")
+    list_of_attacks = list(my_hero['attacks'])
+    removed_attack_1 = list_of_attacks.pop(1)
+    removed_attack_2 = list_of_attacks.pop(1)
+    my_hero['attacks'] = tuple(list_of_attacks)
+    slow_print(f"{removed_attack_1} and {removed_attack_2} have been removed")
+
 def run_game():
     slow_print("")
-    slow_print(f"{my_hero['name']}, you wield the triforce of courage. \nYou must embark on a quest to defeat Gannon, who holds the triforce of power, \nand rescue princess zelda, who holds the triforce of wisdom.")
+    slow_print(f"{my_hero['name']}, you wield the triforce of courage. \nYou must embark on a quest to defeat Gannon, an evil demon who holds the triforce of power, \nand rescue princess zelda, who holds the triforce of wisdom.")
     slow_print("")
-    slow_print(f"{my_hero['name']} has traveled to fire mountain and encountered {villain_1['name']} (Level: {villain_1['level']} Health: {villain_1['health']})")
+    slow_print(f"{my_hero['name']} has traveled to fire mountain and encountered {villain_1['name']}, a giant fire breathing serpent. (Level: {villain_1['level']} Health: {villain_1['health']})")
     slow_print("")
     battle(villain_1)
     if my_hero['health'] <= 0:
@@ -72,9 +73,9 @@ def run_game():
     slow_print("")
     loot_rupies(villain_1)
     slow_print("")
-    level_up(villain_1)
+    level_up()
     slow_print("")
-    slow_print(f"{my_hero['name']} has traveled to the spirit temple and encountered {villain_2['name']} (Level: {villain_2['level']} Health: {villain_2['health']})")
+    slow_print(f"{my_hero['name']} has traveled to the spirit temple and encountered {villain_2['name']}, magical witches wielding the elements. (Level: {villain_2['level']} Health: {villain_2['health']})")
     slow_print("")
     battle(villain_2)
     if my_hero['health'] <= 0:
@@ -83,7 +84,7 @@ def run_game():
     slow_print("")
     loot_rupies(villain_2)
     slow_print("")
-    level_up(villain_2)
+    level_up()
     slow_print("")
     remove_useless_attacks()
     slow_print("")
@@ -96,7 +97,7 @@ def run_game():
     slow_print("")
     loot_rupies(villain_3)
     slow_print("")
-    level_up(villain_3)
+    level_up()
     if my_hero['health'] > 0:
         slow_print("")
         slow_print("Congratulations, you have defeated Gannon and regained the triforce of power!\nYou, along with Princess Zelda and her triforce of wisdom \nwill keep balance and peace in Hyrule.")
